e8d39f1d789bca3031f39b9701e1e197dc9ea6db
harisrizwan1 <harisrizwan985@gmail.com>
2021-11-09 11:40:39 -0600

    added lastIndexOf.js

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = function (input, num) {
+  let output = -1;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === num) {
+      output = i;
+    }
+  }
+  return output;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
