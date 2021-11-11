514fdb19f8ef93a8624f69b85ad34038a8cb8e01
harisrizwan1 <harisrizwan985@gmail.com>
2021-11-11 08:49:39 -0600

    updated loopylighthosue.js

---
+++ concat.js
@@ -0,0 +1,11 @@
+const concat = function (first, second) {
+  for (let i of second) {
+    first.push(i);
+  }
+  return first;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,13 +1,17 @@
-for (let i = 100; i <= 200; i++) {
-  if (i % 3 === 0) {
-    if (i % 4 === 0) {
-      console.log("LoopyLighthouse");
+const loopyLighthouse = function (range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0) {
+      if (i % multiples[1] === 0) {
+        console.log(words[0] + words[1]);
+      } else {
+        console.log(words[0]);
+      }
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
     } else {
-      console.log("Loopy");
+      console.log(i);
     }
-  } else if (i % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(i);
   }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
